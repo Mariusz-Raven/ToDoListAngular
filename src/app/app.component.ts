ae87e3be8ba5efa0a060451caac2c28a81f33399
Mario <kruczeck@wp.pl>
2021-12-13 22:50:50 +0100

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  config: { [key: string]: string } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2022-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2022-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2022-01-04',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+
+
+  createTask(name: string, deadline: string) {
+    const task: Task = {
+      name: name,
+      deadline: deadline,
+      done: false,
+    };
+    this.tasks.push(task);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
