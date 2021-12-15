9bf1ee97859b390a6f76c139d5b82114f2327f3a
Mario <kruczeck@wp.pl>
2021-12-15 21:00:35 +0100

    comlete App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,24 @@ import { Task } from './task';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  config: { [key: string]: string } | null = null;
+  editMode = false;
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string } | null  = null;
   tasks: Task[] = [
     {
       name: 'Siłownia',
-      deadline: '2022-01-02',
+      deadline: '2020-01-02',
       done: false,
     },
     {
       name: 'Nauka Angulara',
-      deadline: '2022-01-03',
-      done: false,
+      deadline: '2020-01-03',
+      done: true,
     },
     {
       name: 'Sprzątanie kuwety',
-      deadline: '2022-01-04',
+      deadline: '2020-01-04',
       done: false,
     },
   ];
@@ -34,20 +37,39 @@ export class AppComponent {
         date: new Date().toDateString(),
       };
     }, 500);
+    this.sortTasks();
   }
 
   clearTasks() {
     this.tasks = [];
   }
 
-
-
-  createTask(name: string, deadline: string) {
+  createTask() {
     const task: Task = {
-      name: name,
-      deadline: deadline,
+      name: this.taskName,
+      deadline: this.taskDate,
       done: false,
     };
     this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTasks();
+  }
+
+  switchEditMode(){
+    this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task : Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(e => e !== task);
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) => a.done === b.done ? 0 : a.done ? 1 : -1);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import localePl from '@angular/common/locales/pl';
 import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 registerLocaleData(localePl);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
